eb447ae44a8941018f5a7368ce7e288fc9c8eb36
Lucy Park <me@lucypark.kr>
2014-07-23 17:40:38 +0900

    Add color loader

--- utils.py
+++ utils.py
@@ -1,7 +1,15 @@
 #! /usr/bin/python2.7
 # -*- coding: utf-8 -*-
 
+import json
 import struct
 
 def hex2rgb(hexstr):
     return struct.unpack('BBB', hexstr.decode('hex'))
+
+def popong_colors(colortype='hex'):
+    with open('static/colors.json', 'r') as f:
+        d = json.load(f)
+    if colortype=='rgb':
+        d = { k: hex2rgb(v[1:]) for k, v in d.items() }
+    return d
