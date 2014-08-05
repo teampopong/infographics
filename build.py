a1acac8e202316372265da5c016867ee747d6682
Lucy Park <me@lucypark.kr>
2014-08-05 23:05:14 +0900

    Add drawing builder

---
+++ build.py
@@ -0,0 +1,27 @@
+#! /usr/bin/python2.7
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+
+from PIL import Image
+
+
+def check_input():
+    if not len(sys.argv) > 1:
+        raise IOError('Usage: ./build.py [year]/[title]')
+
+    if not os.path.isdir(sys.argv[1]):
+        raise IOError('Path does not exist')
+
+def draw_graph():
+    os.chdir(sys.argv[1])
+    runfile = [f for f in os.listdir('.')\
+                 if os.path.basename(f).rsplit('.', 1)[0]=='draw'][0]
+    os.system('./%s' % runfile)
+    Image.open('drawing.png').show()
+
+
+if __name__=='__main__':
+    check_input()
+    draw_graph()
